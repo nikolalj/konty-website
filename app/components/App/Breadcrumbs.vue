1d10194579c029b9915cfb07710de5938ff45344
nikolalj <nikola.ljumovic@gmail.com>
2025-09-04 00:23:40 +0200

    updates

--- app/components/App/Breadcrumbs.vue
+++ app/components/App/Breadcrumbs.vue
@@ -8,18 +8,14 @@ import { useRuntimeConfig } from '#imports'
 const config = useRuntimeConfig()
 const items = useBreadcrumbItems()
 
-const breadcrumbSchema = computed(() => ({
-  itemListElement: items.value.map((item, index) => ({
-    '@type': 'ListItem',
-    position: index + 1,
-    name: item.label,
-    item: `${config.public.siteUrl}${item.to}`
-  }))
-}))
-
-watch(breadcrumbSchema, val => {
-  useSchemaOrg([
-    defineBreadcrumb(val)
-  ])
-}, { immediate: true })
+useSchemaOrg([
+  defineBreadcrumb({
+    itemListElement: items.value.map((item, index) => ({
+      '@type': 'ListItem',
+      position: index + 1,
+      name: item.label,
+      item: `${config.public.siteUrl}${item.to}`
+    }))
+  })
+])
 </script>
