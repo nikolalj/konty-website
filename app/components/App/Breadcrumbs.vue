9775b3ab8200006a3b92e452434932fb877e4034
nikolalj <nikola.ljumovic@gmail.com>
2025-09-06 18:26:01 +0200

    updates

--- app/components/App/Breadcrumbs.vue
+++ app/components/App/Breadcrumbs.vue
@@ -3,19 +3,7 @@
 </template>
 
 <script setup lang="ts">
-import { useRuntimeConfig } from '#imports'
-
-const config = useRuntimeConfig()
-const items = useBreadcrumbItems()
-
-useSchemaOrg([
-  defineBreadcrumb({
-    itemListElement: items.value.map((item, index) => ({
-      '@type': 'ListItem',
-      position: index + 1,
-      name: item.label,
-      item: `${config.public.siteUrl}${item.to}`
-    }))
-  })
-])
+const items = useBreadcrumbItems({
+  schemaOrg: true
+})
 </script>
