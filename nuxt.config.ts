d2aea3d52f1b5481e3420294956ff6f588803d65
nikolalj <nikola.ljumovic@gmail.com>
2025-09-07 21:51:26 +0200

    updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,7 @@ export default defineNuxtConfig({
     build: {
       minify: 'terser',
       cssMinify: true,
+      cssCodeSplit: false,
       rollupOptions: {
         output: {
           manualChunks: {
